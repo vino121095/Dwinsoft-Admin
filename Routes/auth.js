ede252d6b48fe103a58d91d4367bcac06e0dccf7
vino121095 <168708428+vino121095@users.noreply.github.com>
2024-12-31 13:33:27 +0530

    Delete Routes directory

--- Routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { login } = require("../Controllers/auth");
-
-const router = express.Router();
-
-// Login route
-router.post("/login", login);
-
-module.exports = router;
