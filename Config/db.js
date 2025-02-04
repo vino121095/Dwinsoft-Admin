721b0e3b161f6fa6c468f70ee79d8cb0ff1d8014
Selvaganesh K <selvaganesh2608@gmail.com>
2025-02-04 11:06:51 +0530

    db changes | 04-02

--- Config/db.js
+++ Config/db.js
@@ -1,17 +1,17 @@
 const { Sequelize } = require('sequelize');  // Use destructuring to import Sequelize
 
-// const sequelize = new Sequelize('dwinsoft', 'root', 'pass12345', {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306,  // Port should be a number, not a string
-// });
+const sequelize = new Sequelize('dwinsoft', 'root', 'pass12345', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,  // Port should be a number, not a string
+});
 
 // DON'T UNCOMMENT OR DELETE THIS CODE
-const sequelize = new Sequelize('dwinsoft', 'postgres', '12345', {
-        host: 'localhost',
-        dialect: 'postgres',
-        port: 5432,
-    });
+// const sequelize = new Sequelize('dwinsoft', 'postgres', '12345', {
+//         host: 'localhost',
+//         dialect: 'postgres',
+//         port: 5432,
+//     });
 
 async function testConnection() {
     try {
